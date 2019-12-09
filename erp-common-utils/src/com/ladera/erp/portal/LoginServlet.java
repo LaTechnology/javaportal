c422b95d36476cd4e9b572a20e012700232b55dc
selvaraja143 <selva.raj@laderatechnology.com>
2019-12-09 09:08:55 +0530

    Feature #1234

---
+++ erp-common-utils/src/com/ladera/erp/portal/LoginServlet.java
@@ -0,0 +1,18 @@
+/**
+ * 
+ */
+package com.ladera.erp.portal;
+
+import javax.net.ssl.HttpsURLConnection;
+
+/**
+ * @author admin
+ *
+ */
+public class LoginServlet extends Object{
+
+	public static void name() {
+		int a=0;
+		System.out.println(""+a);
+	}
+}
