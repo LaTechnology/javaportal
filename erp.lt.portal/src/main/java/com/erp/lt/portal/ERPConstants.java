b3323f81ec4d2d486faf80997395b6af68279c5e
sidhharth25 <sidhharth.govardhanan@laderatechnology.com>
2019-12-28 09:05:22 +0530

    communication details code

---
+++ erp.lt.portal/src/main/java/com/erp/lt/portal/ERPConstants.java
@@ -0,0 +1,22 @@
+/**
+ * 
+ */
+package com.erp.lt.portal;
+
+/**
+ * @author User
+ *
+ */
+public class ERPConstants {
+	public static final String URL_BASE = "/api/v1";
+	public static final String COMMUNICATION_DETAILS_ADD_URL = "/add/com/details/";
+	public static final String COMMUNICATION_DETAILS_GET_URL = "/get/com/details/{empId}";
+	public static final String COMMUNICATION_DETAILS_EDIT_URL = "/edit/com/details";
+	public static final String COMMUNICATION_DETAILS_PATCHING_URL = "/patching/com/details";
+	public static final String COMMUNICATION_DETAILS_DELETE_URL = "/delete/com/details/{empId}";
+	public static final String MOBILE_DETAILS_ADD_URL = "/add/mob/details";
+	public static final String MOBILE_DETAILS_GET_URL = "/get/mob/details/{code}";
+	public static final String MOBILE_DETAILS_EDIT_URL = "/edit/mob/details";
+	public static final String MOBILE_DETAILS_PATCHING_URL = "/patching/mob/details";
+	public static final String MOBILE_DETAILS_DELETE_URL = "/delete/{code}";
+}
