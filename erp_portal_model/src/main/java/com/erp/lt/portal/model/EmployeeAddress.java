2560b8669459d127cc36c9b29552f508c16b489c
srinath1793 <srinath.sathyanarayanan@laderatechnology.com>
2020-01-04 16:39:31 +0530

    Model updated - Selva

--- erp_portal_model/src/main/java/com/erp/lt/portal/model/EmployeeAddress.java
+++ erp_portal_model/src/main/java/com/erp/lt/portal/model/EmployeeAddress.java
@@ -4,52 +4,49 @@ import java.io.Serializable;
 import javax.persistence.*;
 import java.util.Date;
 
-
 /**
  * The persistent class for the employee_address database table.
  * 
  */
 @Entity
-@Table(name="employee_address")
-@NamedQuery(name="EmployeeAddress.findAll", query="SELECT e FROM EmployeeAddress e")
+@Table(name = "employee_address")
+@NamedQuery(name = "EmployeeAddress.findAll", query = "SELECT e FROM EmployeeAddress e")
 public class EmployeeAddress implements Serializable {
 	private static final long serialVersionUID = 1L;
 
 	@Id
-	@GeneratedValue(strategy=GenerationType.IDENTITY)
-	@Column(name="address_code")
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "address_code")
 	private int addressCode;
 
-	@Column(name="address_line_1")
+	@Column(name = "address_line_1")
 	private String addressLine1;
 
-	@Column(name="address_line_2")
+	@Column(name = "address_line_2")
 	private String addressLine2;
 
 	@Temporal(TemporalType.DATE)
-	@Column(name="begin_date")
+	@Column(name = "begin_date")
 	private Date beginDate;
 
 	private String city;
 
 	private String country;
 
-	@Column(name="employee_code")
+	@Column(name = "employee_code")
 	private int employeeCode;
 
 	@Temporal(TemporalType.DATE)
-	@Column(name="end_date")
+	@Column(name = "end_date")
 	private Date endDate;
-
-	private int idEmployee_address;
 
 	private String pincode;
 
 	private String state;
 
-	//bi-directional many-to-one association to AddressType
-	@ManyToOne(fetch=FetchType.LAZY)
-	@JoinColumn(name="address_type_code")
+	// bi-directional many-to-one association to AddressType
+	@ManyToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name = "address_type_code")
 	private AddressType addressType;
 
 	public EmployeeAddress() {
@@ -117,14 +114,6 @@ public class EmployeeAddress implements Serializable {
 
 	public void setEndDate(Date endDate) {
 		this.endDate = endDate;
-	}
-
-	public int getIdEmployee_address() {
-		return this.idEmployee_address;
-	}
-
-	public void setIdEmployee_address(int idEmployee_address) {
-		this.idEmployee_address = idEmployee_address;
 	}
 
 	public String getPincode() {
