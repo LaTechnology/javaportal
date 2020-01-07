0f555f4ae6a3de1f32698cb5e5df48fcae64c9bf
srinath1793 <srinath.sathyanarayanan@laderatechnology.com>
2020-01-07 20:16:37 +0530

    portal dashboard added - srinath

---
+++ erp-portal-dashboard/src/main/java/com/lt/portal/dashboard/ErpPortalDashboardApplication.java
@@ -0,0 +1,13 @@
+package com.lt.portal.dashboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ErpPortalDashboardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ErpPortalDashboardApplication.class, args);
+	}
+
+}
